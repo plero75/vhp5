6a7c3e42d1acf5322196e38c9dca26678e236388
plop14 <70171493+plero75@users.noreply.github.com>
2025-07-03 19:21:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  proxy: "https://ton-proxy.workers.dev/?url=",
+  proxy: "https://ratp-proxy.hippodrome-proxy42.workers.dev/?url=",
   stops: {
     rer: "STIF:StopArea:SP:43135:",
     bus77: "STIF:StopArea:SP:463641:",
